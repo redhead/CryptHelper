933ae6cc6b8bfd6a6384103444e31842c0ff66c0
Radek Ježdík <redhead@email.cz>
2012-02-22 22:14:47 +0100

    added license

--- src/cz/cvut/kbe/crypthelper/ui/MainWindow.java
+++ src/cz/cvut/kbe/crypthelper/ui/MainWindow.java
@@ -1,7 +1,39 @@
 /*
+ * Software License Agreement (BSD License)
+ * 
+ * Copyright (c) 2012, Radek Ježdík <jezdik.radek@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use of this software in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *   Redistributions of source code must retain the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer.
+ * 
+ *   Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer in the documentation and/or other
+ *   materials provided with the distribution.
+ * 
+ *   Neither the name of Radek Ježdík <jezdik.radek@gmail.com> nor the names of its
+ *   contributors may be used to endorse or promote products
+ *   derived from this software without specific prior
+ *   written permission by Radek Ježdík <jezdik.radek@gmail.com>
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package cz.cvut.kbe.crypthelper.ui;
 
+import cz.cvut.kbe.crypthelper.CharMap;
+import cz.cvut.kbe.crypthelper.CharMap.CharEntry;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -9,8 +41,6 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.table.DefaultTableCellRenderer;
-import cz.cvut.kbe.crypthelper.CharMap;
-import cz.cvut.kbe.crypthelper.CharMap.CharEntry;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -68,7 +98,7 @@ public class MainWindow extends javax.swing.JFrame {
 		graphPanel.removeAll();
 		graphPanel.add(chartPanel, BorderLayout.CENTER);
 		graphPanel.updateUI();
-		
+
 		bottomPanel.setVisible(true);
 	}
 
